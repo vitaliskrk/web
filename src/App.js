1c91489104e98dcf4d54aafacb041c0281bfa1ec
vitaliskrk <vitaliipavlovmail@gmail.com>
2022-04-19 19:17:46 +0200

    leletka haha

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Селедка с луком</h1>
+        <h1>leletka</h1>
       </header>
     </div>
   );
